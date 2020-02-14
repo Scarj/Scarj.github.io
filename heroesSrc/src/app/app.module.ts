92ef61d5b9b63538421d5efac15f5ba201aed6f1
Evgeniy Scherbakov <evgeniy.scherbakov@novardis.com>
2020-02-14 17:16:26 +0300

    AW-007: Tour of Heroes App: Get data from a server

--- heroesSrc/src/app/app.module.ts
+++ heroesSrc/src/app/app.module.ts
@@ -1,14 +1,18 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {BrowserModule} from '@angular/platform-browser';
+import {HttpClientModule} from "@angular/common/http";
 import {APP_BASE_HREF} from "@angular/common";
-import { HeroesComponent } from './components/heroes/heroes.component';
+import {NgModule} from '@angular/core';
 import {FormsModule} from "@angular/forms";
-import { HeroDetailComponent } from './components/hero-detail/hero-detail.component';
-import { MessagesComponent } from './components/messages/messages.component';
-import { DashboardComponent } from './components/dashboard/dashboard.component';
+
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {HeroesComponent} from './components/heroes/heroes.component';
+import {HeroDetailComponent} from './components/hero-detail/hero-detail.component';
+import {MessagesComponent} from './components/messages/messages.component';
+import {DashboardComponent} from './components/dashboard/dashboard.component';
+import {HttpClientInMemoryWebApiModule} from "angular-in-memory-web-api";
+import { InMemoryDataService }  from './services/in-memory-data.service';
+import { HeroSearchComponent } from './components/hero-search/hero-search.component';
 
 @NgModule({
   declarations: [
@@ -16,12 +20,15 @@ import { DashboardComponent } from './components/dashboard/dashboard.component';
     HeroesComponent,
     HeroDetailComponent,
     MessagesComponent,
-    DashboardComponent
+    DashboardComponent,
+    HeroSearchComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule,
+    HttpClientInMemoryWebApiModule.forRoot(InMemoryDataService, { dataEncapsulation: false })
   ],
   providers: [{
     provide: APP_BASE_HREF, useValue: '/heroes'
--- heroesSrc/src/app/components/heroes/heroes.component.ts
+++ heroesSrc/src/app/components/heroes/heroes.component.ts
@@ -11,7 +11,6 @@ import {MessageService} from "../../services/message.service";
 })
 export class HeroesComponent implements OnInit {
   heroes: Hero[];
-  selectedHero: Hero;
 
   hero: Hero = {
     id: 1,
@@ -29,5 +28,21 @@ export class HeroesComponent implements OnInit {
     this.heroService.getHeroes().subscribe(heroes => {
       this.heroes = heroes;
     });
+  }
+
+  add(name: string): void {
+    name = name.trim();
+    if (!name) {
+      return;
+    }
+    this.heroService.addHero({name} as Hero)
+      .subscribe(hero => {
+        this.heroes.push(hero);
+      });
+  }
+
+  delete(hero: Hero): void {
+    this.heroes = this.heroes.filter(h => h !== hero);
+    this.heroService.deleteHero(hero).subscribe();
   }
 }
